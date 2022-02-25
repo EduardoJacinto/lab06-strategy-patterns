632f9b8dee525d3e050b646317ec419d3fe40a23
zamyy78 <zamyy78@gmail.com>
2022-02-25 01:06:54 -0800

    Add unit tests

--- test.cpp
+++ test.cpp
@@ -51,6 +51,37 @@ TEST(printTest, Cases) {
 	EXPECT_EQ(out.str(), out2.str());
 }
 
+TEST(printTest, Substring) {
+        Spreadsheet sheet1;
+        Spreadsheet sheet2;
+        stringstream out;
+        stringstream out2;
+
+        sheet1.set_column_names(("First", "Last", "Age", "Major"));
+        sheet1.add_row(("Diane", "apple", "22", "business"));
+        sheet1.add_row(("David", "apples", "21", "computer science"));
+	sheet1.add_row(("Sarah", "Snapple", "22", "computer science"));
+	sheet1.add_row(("George", "APPLE", "21", "astrophysics"));
+	sheet1.add_row(("Joe", "app", "21", "mathematics"));
+        sheet1.set_selection(new Select_Contains(&sheet, "Last", "apple"));
+
+        sheet1.print_selection(out);
+
+        sheet2.set_column_names(("First", "Last", "Age", "Major"));
+        sheet2.add_row(("Diane", "dole", "22", "business"));
+        sheet2.add_row(("David", "Dole", "21", "computer science"));
+	sheet2.add_row(("Sarah", "Snapple", "22", "computer science"));
+        sheet2.add_row(("George", "APPLE", "21", "astrophysics"));
+        sheet2.add_row(("Joe", "app", "21", "mathematics"));
+
+        sheet2.set_selection(new Select_Contains(&sheet, "Last", "apple"));
+
+        sheet2.print_selection(out2);
+
+        EXPECT_EQ(out.str(), out2.str());
+}
+
+
 
 
 
