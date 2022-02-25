a86cf721694e5526b9a9e3add6e74a6174273d5e
EduardoJacinto <ejaci.003@ucr.edu>
2022-02-25 02:36:11 -0800

    created function tests for select contains and check output

--- test.cpp
+++ test.cpp
@@ -8,6 +8,17 @@
 
 using namespace std;
 
+TEST(printTest,testContains) {
+	Spreadsheet sheet;
+
+	sheet.set_column_names({"First","Last","Age","Major"});
+	sheet.add_row({"Amanda","Andrews","22","business"});
+	sheet.add_row({"Brain","Becker","21","computer science"});
+	
+	sheet.print_selection(cout);
+	cout << endl;
+}
+
 TEST(printTest, Empty) {
 	Spreadsheet sheet1;
 	Spreadsheet sheet2;
@@ -30,27 +41,6 @@ TEST(printTest, Empty) {
 	EXPECT_EQ(out.str(), out2.str());
 }
 
-TEST(printTest, Cases) {
-        Spreadsheet sheet1;
-        Spreadsheet sheet2;
-        stringstream out;
-        stringstream out2;
-
-        sheet1.set_column_names({"First", "Last", "Age", "Major"});
-        sheet1.add_row({"Diane", "dole", "22", "business"});
-        sheet1.add_row({"David", "Dole", "21", "computer science"});
-        sheet1.set_selection(new Select_Contains(&sheet1, "Last", "Dole"));
-
-        sheet1.print_selection(out);
-
-        sheet2.set_column_names({"First", "Last", "Age", "Major"});
-        sheet2.add_row({"Diane", "dole", "22", "business"});
-        sheet2.add_row({"David", "Dole", "21", "computer science"});
-        sheet2.print_selection(out2);
-
-	EXPECT_EQ(out.str(), out2.str());
-}
-
 TEST(printTest, Substring) {
         Spreadsheet sheet1;
         Spreadsheet sheet2;
@@ -68,18 +58,13 @@ TEST(printTest, Substring) {
         sheet1.print_selection(out);
 
         sheet2.set_column_names({"First", "Last", "Age", "Major"});
-        sheet2.add_row({"Diane", "dole", "22", "business"});
-        sheet2.add_row({"David", "Dole", "21", "computer science"});
+        sheet2.add_row({"Diane", "apple", "22", "business"});
+        sheet2.add_row({"David", "apples", "21", "computer science"});
 	sheet2.add_row({"Sarah", "Snapple", "22", "computer science"});
-        sheet2.add_row({"George", "APPLE", "21", "astrophysics"});
-        sheet2.add_row({"Joe", "app", "21", "mathematics"});
-
         sheet2.print_selection(out2);
 
         EXPECT_EQ(out.str(), out2.str());
 }
-
-
 
 
 
